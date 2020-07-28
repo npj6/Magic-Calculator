487b8bd0cbf917a05c71e96895e41775548ed180
npj6 <npj6@alu.ua.es>
2020-07-28 12:20:45 +0200

    Project structure changed, unit tests added

--- makefile
+++ makefile
@@ -3,20 +3,27 @@
 CC=g++
 OPTIONS= -g -std=c++11
 LIBDIR=lib
-INCLUDEDIR=include
+SRCDIR=src
+TESTDIR=test
 _OBJ=deckManager.o
-OBJ=$(patsubst %,$(LIBDIR)/%,$(_OBJ))
+OBJ=$(patsubst %,$(SRCDIR)/%,$(_OBJ))
+_TESTOBJ=deckManagerTests.o
+TESTOBJ=$(patsubst %,$(TESTDIR)/%,$(_TESTOBJ))
 
-all: main.cpp $(OBJ)
-	$(CC) $(OPTIONS) -I$(INCLUDEDIR) main.cpp $(OBJ) -o mtgCalc.exec
+all: $(SRCDIR)/main.cpp $(OBJ)
+	$(CC) $(OPTIONS) -I$(LIBDIR) $< $(OBJ) -o mtgCalc.exec
 
-test: $(INCLUDEDIR)/catch.hpp
-	echo exec the test > test.exec
+test: $(TESTDIR)/test.cpp $(LIBDIR)/catch2/catch.hpp $(OBJ) $(TESTOBJ)
+	$(CC) $(OPTIONS) -I$(LIBDIR) $< $(OBJ) $(TESTOBJ) -o test.exec
+	./test.exec
 
-$(LIBDIR)/%.o: $(LIBDIR)/%.cpp $(INCLUDEDIR)/%.h
-	$(CC) $(OPTIONS) -c -I$(INCLUDEDIR) -o $@ $<
+$(TESTDIR)/%.o: $(TESTDIR)/%.cpp
+	$(CC) $(OPTIONS) -c -I$(LIBDIR) -o $@ $<
 
-$(INCLUDEDIR)/catch.hpp:
+$(SRCDIR)/%.o: $(SRCDIR)/%.cpp
+	$(CC) $(OPTIONS) -c -I$(LIBDIR) -o $@ $<
+
+$(LIBDIR)/catch2/catch.hpp:
 	wget -O $@ https://raw.githubusercontent.com/catchorg/Catch2/master/single_include/catch2/catch.hpp
 
 clean:
---
+++ test/deckManagerTests.cpp
@@ -0,0 +1,46 @@
+#include "catch2/catch.hpp"
+#include "deckManager.h"
+
+TEST_CASE("Decklist is modified") {
+  MTGCALC::DeckManager dm;
+  REQUIRE(dm.deckList() == "");
+
+  dm.addCard("Card 1", 2);
+  REQUIRE(dm.deckList() == "#0 2 Card 1\n");
+
+  dm.addCard("Card 2", 3);
+  REQUIRE(dm.deckList() == "#0 2 Card 1\n#1 3 Card 2\n");
+
+  dm.addCard("Card 1", 2);
+  REQUIRE(dm.deckList() == "#0 4 Card 1\n#1 3 Card 2\n");
+
+  SECTION("Cards are deleted") {
+    dm.deleteCard(1);
+    REQUIRE(dm.deckList() == "#0 4 Card 1\n");
+
+    dm.addCard("Card 2", 3);
+    REQUIRE(dm.deckList() == "#0 4 Card 1\n#1 3 Card 2\n");
+
+    dm.deleteCard(0);
+    REQUIRE(dm.deckList() == "#0 3 Card 2\n");
+
+    dm.deleteCard(0);
+    REQUIRE(dm.deckList() == "");
+  }
+
+  SECTION("Cards are modified") {
+    dm.modifyCard(0, 2);
+    REQUIRE(dm.getCard(0).copies == 2);
+
+    dm.modifyCard(0, "Birds of Paradise");
+    REQUIRE(dm.getCard(0).name == "Birds of Paradise");
+
+    dm.modifyCard(1, 2);
+    REQUIRE(dm.getCard(1).copies == 2);
+
+    dm.modifyCard(1, "Forest");
+    REQUIRE(dm.getCard(1).name == "Forest");
+
+    REQUIRE(dm.deckList() == "#0 2 Birds of Paradise\n#1 2 Forest\n");
+  }
+}
